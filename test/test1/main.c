f3827c99938647984d2678552a1b749a5a6229bf
Luca Deltodesco <luca@deltaluca.me.uk>
2011-09-13 13:26:59 +0100

    added results of test1 for reference

---
+++ test/test1/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h> 
+#include <math.h> 
+#include <main.h> 
+
+int main(int *argc,char* *argv)
+{
+  int __retv;
+  int xs[11];
+  int i;
+  int sum;
+  sum = 0;
+  i = 0;
+  int __fbound0 = 9;
+  for (; i <= __fbound0; ++i) {
+    xs[i] = i;
+    printf("%d\n",xs[i]);
+    sum = sum + i;
+  }
+  printf("%s%d\n","and their sum is: ",sum);
+  __retv = 0;
+  return __retv;
+}
