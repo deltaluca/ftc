72b5c682ac65110ef6c22715db52fa458187656c
Luca Deltodesco <luca@deltaluca.me.uk>
2011-09-13 13:25:16 +0100

    Added first test case

---
+++ test/test1/main.f90
@@ -0,0 +1,21 @@
+integer(4) function main(argc, argv)
+
+    integer(4) :: argc
+    character(len=*), dimension(*) :: argv
+
+    integer(4), dimension(0:10) :: xs
+    integer(4) :: i, sum
+
+    sum = 0
+
+    do i = 0,9
+       xs(i) = i
+       print *, xs(i)
+       sum = sum + i
+    enddo
+
+    print *, "and their sum is: ", sum
+
+    main = 0
+    return
+end
